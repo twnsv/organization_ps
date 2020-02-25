c4dc33201a759555bb4f66c1354e1c06b8b74d9f
wen <alejet.larvata@gmail.com>
2020-02-25 16:06:03 +0800

    chore Add module OrganizationPs

--- lib/organization_ps.rb
+++ lib/organization_ps.rb
@@ -1,5 +1,27 @@
-require "organization_ps/engine"
-
 module OrganizationPs
-  # Your code goes here...
+  require "organization_ps/engine"
+
+  mattr_accessor :organization_class
+  mattr_accessor :tree_node_class
+  mattr_accessor :user_class
+
+  def self.organization_class
+    @@organization_class.constantize
+  end
+
+  def self.tree_node_class
+    @@tree_node_class.constantize
+  end
+
+  def self.user_class
+    @@user_class.constantize
+  end
+
+  require 'organization/factory/nodes'
+  require 'organization/factory/orgs'
+  require 'organization/trunk/growth'
+  require 'organization/finder/orgs'
+  require 'organization/finder/users'
+  require 'organization/finder/authorities'
+  require 'organization/service/org_nodes'
 end
